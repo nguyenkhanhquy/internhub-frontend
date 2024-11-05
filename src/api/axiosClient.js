3d20b03ba497f53feed8c86c8b094f6b07c52ee7
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-11-06 05:28:47 +0700

    add: axios client

---
+++ src/api/axiosClient.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { getToken } from "../services/localStorage.service";
+
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+axiosClient.interceptors.request.use(
+    (config) => {
+        const accessToken = getToken();
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default axiosClient;
