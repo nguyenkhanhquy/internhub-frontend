0ba7ebb3cfe6c5db8f4fb9095b02efc8d4bbbb9a
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-11-06 05:56:25 +0700

    add: auth service

---
+++ src/api/constants.js
@@ -0,0 +1,9 @@
+export const AUTH_API = {
+    LOGIN: "/auth/login",
+    LOGOUT: "/auth/logout",
+    ME: "/auth/me",
+};
+
+export const USERS_API = {
+    UPDATE_PASSWORD: "/users/update-password",
+};
---
+++ src/services/auth.service.js
@@ -0,0 +1,28 @@
+import { axiosClient } from "../api/axiosClient";
+import { AUTH_API } from "../api/constants";
+
+export const login = async (email, password) => {
+    return axiosClient
+        .post(AUTH_API.LOGIN, {
+            email: email,
+            password: password,
+        })
+        .then((response) => response?.data)
+        .catch((error) => error?.response?.data);
+};
+
+export const logout = async (accessToken) => {
+    return axiosClient
+        .post(AUTH_API.LOGOUT, {
+            accessToken: accessToken,
+        })
+        .then((response) => response?.data)
+        .catch((error) => error?.response?.data);
+};
+
+export const getAuthUser = async () => {
+    return axiosClient
+        .get(AUTH_API.ME)
+        .then((response) => response?.data)
+        .catch((error) => error?.response?.data);
+};
