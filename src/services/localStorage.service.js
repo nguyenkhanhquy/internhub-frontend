439f0621295d0678f8aed74040dae8e953a6c43d
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-11-06 04:47:49 +0700

    add: local storage service

---
+++ src/services/localStorage.service.js
@@ -0,0 +1,32 @@
+export const KEY_TOKEN = "accessToken";
+export const EXPIRATION_TIME = "tokenExpiration";
+
+export const setToken = (accessToken) => {
+    const expirationTime = new Date().getTime() + 3600 * 1000;
+    localStorage.setItem(KEY_TOKEN, accessToken);
+    localStorage.setItem(EXPIRATION_TIME, expirationTime);
+};
+
+export const getToken = () => {
+    const accessToken = localStorage.getItem(KEY_TOKEN);
+    const expirationTime = localStorage.getItem(EXPIRATION_TIME);
+
+    if (!accessToken || !expirationTime) {
+        return null;
+    }
+
+    const now = new Date().getTime();
+
+    if (now > expirationTime) {
+        localStorage.removeItem(KEY_TOKEN);
+        localStorage.removeItem(EXPIRATION_TIME);
+        return null;
+    }
+
+    return accessToken;
+};
+
+export const removeToken = () => {
+    localStorage.removeItem(KEY_TOKEN);
+    localStorage.removeItem(EXPIRATION_TIME);
+};
