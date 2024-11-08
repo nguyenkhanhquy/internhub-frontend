55374508a83b6b52a03930e5183bf986549ccc6b
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-08 08:44:14 +0700

    add: Job Details Page

---
+++ src/pages/JobDetailsPage/JobDetailsPage.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../../layouts/MainLayout/MainLayout";
+
+const JobDetailsPage = () => {
+    return (
+        <MainLayout title="Chi tiết công việc">
+            <h1 className="text-3xl font-bold underline">Chi tiết công việc</h1>
+        </MainLayout>
+    );
+};
+
+export default JobDetailsPage;
