1d7d69cbf3f8d1e2aabacc0d050d53c56dc11ff7
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-12 19:00:35 +0700

    update: Account Layout

--- src/components/navigations/AccountNavigation/AccountNavigation.jsx
+++ src/components/navigations/AccountNavigation/AccountNavigation.jsx
@@ -1,7 +1,9 @@
 import { List, ListItemButton, ListItemText, ListItemIcon, Typography } from "@mui/material";
 import { useNavigate, useLocation } from "react-router-dom";
 import { AccountCircle, Lock } from "@mui/icons-material";
+import { Paper, Box, Divider, Avatar } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
+import logo from "/images/ute_logo_c.png";
 
 const AccountNavigation = () => {
     const navigate = useNavigate();
@@ -12,51 +14,121 @@ const AccountNavigation = () => {
     };
 
     return (
-        <>
-            <Typography variant="h6" fontWeight="bold" mb={2}>
-                Tài khoản của tôi
-            </Typography>
-            <List sx={{ width: 240 }}>
+        <Paper
+            sx={{
+                bgcolor: "background.paper",
+                borderRadius: 1,
+                boxShadow: "0px 4px 8px rgba(0,0,0,0.2)",
+                textAlign: "center",
+            }}
+        >
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                sx={{
+                    paddingY: 5,
+                    paddingX: 5,
+                }}
+            >
+                {/* Avatar sinh viên */}
+                <Avatar
+                    src={logo}
+                    alt={""}
+                    sx={{
+                        width: "50%",
+                        height: "50%",
+                        marginBottom: 2,
+                        boxShadow: "0px 4px 8px rgba(0,0,0,0.2)", // Đổ bóng cho avatar
+                    }}
+                />
+                <Typography variant="h6" fontWeight="bold" sx={{ minWidth: 200 }}>
+                    Tài khoản của tôi
+                </Typography>
+            </Box>
+            <List>
+                <Divider />
                 <ListItemButton
                     onClick={() => handleNavigate("/account/profile")}
-                    selected={location.pathname === "/account/profile"}
+                    // selected={location.pathname === "/account/profile"}
                     sx={{
-                        color: location.pathname === "/account/profile" ? "primary.main" : "text.primary",
+                        position: "relative",
+                        "&::after": {
+                            content: '""',
+                            position: "absolute",
+                            left: 0,
+                            top: 0,
+                            height: location.pathname === "/account/profile" ? "100%" : "0%",
+                            width: "4px",
+                            backgroundColor: "primary.main",
+                            transition: "height 0.3s ease",
+                        },
+                        "&:hover::after": {
+                            height: "100%",
+                        },
                     }}
                 >
                     <ListItemIcon>
-                        <PersonIcon color={location.pathname === "/account/profile" ? "primary" : "inherit"} />
+                        <PersonIcon />
                     </ListItemIcon>
                     <ListItemText primary="Chi tiết hồ sơ" />
                 </ListItemButton>
 
+                <Divider />
                 <ListItemButton
                     onClick={() => handleNavigate("/account/update-password")}
-                    selected={location.pathname === "/account/update-password"}
+                    // selected={location.pathname === "/account/update-password"}
                     sx={{
-                        color: location.pathname === "/account/update-password" ? "primary.main" : "text.primary",
+                        position: "relative",
+                        "&::after": {
+                            content: '""',
+                            position: "absolute",
+                            left: 0,
+                            top: 0,
+                            height: location.pathname === "/account/update-password" ? "100%" : "0%",
+                            width: "4px",
+                            backgroundColor: "primary.main",
+                            transition: "height 0.3s ease",
+                        },
+                        "&:hover::after": {
+                            height: "100%",
+                        },
                     }}
                 >
                     <ListItemIcon>
-                        <Lock color={location.pathname === "/account/update-password" ? "primary" : "inherit"} />
+                        <Lock />
                     </ListItemIcon>
                     <ListItemText primary="Đổi mật khẩu" />
                 </ListItemButton>
 
+                <Divider />
                 <ListItemButton
                     onClick={() => handleNavigate("/account/details")}
-                    selected={location.pathname === "/account/details"}
+                    // selected={location.pathname === "/account/details"}
                     sx={{
-                        color: location.pathname === "/account/details" ? "primary.main" : "text.primary",
+                        position: "relative",
+                        "&::after": {
+                            content: '""',
+                            position: "absolute",
+                            left: 0,
+                            top: 0,
+                            height: location.pathname === "/account/details" ? "100%" : "0%",
+                            width: "4px",
+                            backgroundColor: "primary.main",
+                            transition: "height 0.3s ease",
+                        },
+                        "&:hover::after": {
+                            height: "100%",
+                        },
                     }}
                 >
                     <ListItemIcon>
-                        <AccountCircle color={location.pathname === "/account/details" ? "primary" : "inherit"} />
+                        <AccountCircle />
                     </ListItemIcon>
                     <ListItemText primary="Chi tiết tài khoản" />
                 </ListItemButton>
             </List>
-        </>
+        </Paper>
     );
 };
 
