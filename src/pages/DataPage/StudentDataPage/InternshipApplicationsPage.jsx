774ae9bed8b05aa129c1324fadcf645064421ab7
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-12-04 08:14:51 +0700

    add: InternshipApplicationsPage.jsx

---
+++ src/pages/DataPage/StudentDataPage/InternshipApplicationsPage.jsx
@@ -0,0 +1,17 @@
+import MainLayout from "../../../layouts/MainLayout/MainLayout";
+import StudentDataLayout from "../../../layouts/DataLayout/StudentDataLayout";
+import PageNavigation from "../../../components/layouts/PageNavigation/PageNavigation";
+import InternshipApplicationsGridView from "../../../components/data/StudentDataGridView/InternShipApplicationsGridView";
+
+const InternshipApplicationsPage = () => {
+    return (
+        <MainLayout title="Đơn đăng ký thực tập">
+            <PageNavigation pageName="Sinh viên" />
+            <StudentDataLayout>
+                <InternshipApplicationsGridView />
+            </StudentDataLayout>
+        </MainLayout>
+    );
+};
+
+export default InternshipApplicationsPage;
