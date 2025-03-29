8df1615d1f275388828c5c5ed3e5189d280e8b93
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2025-03-29 11:35:08 +0700

    update: useWebSocket

--- src/hooks/useWebSocket.js
+++ src/hooks/useWebSocket.js
@@ -5,38 +5,44 @@ import SockJS from "sockjs-client/dist/sockjs";
 
 import { toast } from "react-toastify";
 
+const isDevelopment = import.meta.env.MODE === "development";
+
 const useWebSocket = (id) => {
     const stompClientRef = useRef(null);
     const [flag, setFlag] = useState(false);
 
-    const connectWebSocket = useCallback(() => {
+    const connectWebSocket = useCallback(async () => {
         if (stompClientRef.current) {
-            stompClientRef.current.deactivate(); // Đảm bảo hủy kết nối cũ
+            await stompClientRef.current.deactivate(); // Đảm bảo hủy kết nối cũ
         }
 
         const client = new Client({
             webSocketFactory: () => new SockJS(`${import.meta.env.VITE_BACKEND_URL}` + "/ws"),
-            debug: (str) => console.log(str),
+            debug: (str) => {
+                if (isDevelopment) console.log(str);
+            },
             reconnectDelay: 5000,
             onConnect: () => {
-                console.log("Connected to WebSocket!");
+                if (isDevelopment) console.log("Connected to WebSocket!");
 
                 client.subscribe("/topic/message", (message) => {
                     const receivedMessage = JSON.parse(message.body);
                     toast.info(`Public message: ${receivedMessage.message}`);
                 });
 
-                client.subscribe(`/user/${id}/private`, (message) => {
-                    const receivedMessage = JSON.parse(message.body);
-                    setFlag((prev) => !prev);
-                    toast.info(`${receivedMessage.message}`);
-                });
+                if (id) {
+                    client.subscribe(`/user/${id}/private`, (message) => {
+                        const receivedMessage = JSON.parse(message.body);
+                        setFlag((prev) => !prev);
+                        toast.info(`${receivedMessage.message}`);
+                    });
+                }
             },
             onDisconnect: () => {
-                console.log("Disconnected from WebSocket!");
+                if (isDevelopment) console.log("Disconnected from WebSocket!");
             },
             onStompError: (frame) => {
-                console.error("STOMP error:", frame);
+                if (isDevelopment) console.error("STOMP error:", frame);
             },
         });
 
@@ -44,9 +50,9 @@ const useWebSocket = (id) => {
         stompClientRef.current = client;
     }, [id]);
 
-    const disconnectWebSocket = useCallback(() => {
+    const disconnectWebSocket = useCallback(async () => {
         if (stompClientRef.current) {
-            stompClientRef.current.deactivate();
+            await stompClientRef.current.deactivate();
             stompClientRef.current = null;
         }
     }, []);
