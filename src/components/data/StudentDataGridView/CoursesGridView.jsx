c7181c38c35e045d2e944bcc0c0f575076756664
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2025-05-01 23:49:24 +0700

    update: Courses Table

--- src/components/data/StudentDataGridView/CoursesGridView.jsx
+++ src/components/data/StudentDataGridView/CoursesGridView.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 import { Box, Button } from "@mui/material";
 import CachedIcon from "@mui/icons-material/Cached";
-import GridViewLayout from "../../../layouts/DataLayout/GridViewLayout/GridViewLayout";
+import GridViewLayout from "@layouts/DataLayout/GridViewLayout/GridViewLayout";
 import DataSearchBar from "../DataSearchBar";
 import CoursesTable from "./StudentDataTable/CoursesTable";
+
+import { getAllEnrollmentsByStudent } from "@services/enrollmentService";
 
 const CoursesGridView = () => {
     const [loading, setLoading] = useState(false);
@@ -32,78 +33,26 @@ const CoursesGridView = () => {
         window.open(`/search/${id}`, "_blank");
     };
 
-    // Data mẫu
-    const mockData = [
-        {
-            courseId: 1,
-            courseCode: "INT101",
-            academicYear: "2023-2024",
-            semester: "HK 1",
-            teacherName: "Nguyễn Văn A",
-            finalScore: 8.5,
-            enrollmentStatus: "COMPLETED",
-        },
-        {
-            courseId: 2,
-            courseCode: "INT102",
-            academicYear: "2023-2024",
-            semester: "HK 2",
-            teacherName: "Trần Thị B",
-            finalScore: null,
-            enrollmentStatus: "SUBMITTED",
-        },
-        {
-            courseId: 3,
-            courseCode: "INT103",
-            academicYear: "2022-2023",
-            semester: "HK 1",
-            teacherName: "Trần Thị B",
-            finalScore: null,
-            enrollmentStatus: "NOT_SUBMITTED",
-        },
-        {
-            courseId: 4,
-            courseCode: "INT104",
-            academicYear: "2022-2023",
-            semester: "HK 2",
-            teacherName: "Nguyễn Văn A",
-            finalScore: 4.0,
-            enrollmentStatus: "FAILED",
-        },
-    ];
-
-    // useEffect(() => {
-    //     const fetchSavedJobPosts = async () => {
-    //         setLoading(true);
-    //         try {
-    //             const data = await getAllJobApplyByStudent(currentPage, recordsPerPage, search);
-    //             if (!data.success) {
-    //                 throw new Error(data.message || "Lỗi máy chủ, vui lòng thử lại sau!");
-    //             }
-    //             setTotalPages(data.pageInfo.totalPages);
-    //             setTotalRecords(data.pageInfo.totalElements);
-    //             setCourses(data.result);
-    //         } catch (error) {
-    //             toast.error(error.message);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     };
-
-    //     fetchSavedJobPosts();
-    // }, [search, currentPage, recordsPerPage, flag]);
-
     useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            const filtered = mockData.filter((course) =>
-                course.courseCode.toLowerCase().includes(search.toLowerCase()),
-            );
-            setCourses(filtered);
-            setTotalRecords(filtered.length);
-            setTotalPages(Math.ceil(filtered.length / recordsPerPage));
-            setLoading(false);
-        }, 500);
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const data = await getAllEnrollmentsByStudent(currentPage, recordsPerPage, search);
+                if (!data.success) {
+                    throw new Error(data.message || "Lỗi máy chủ, vui lòng thử lại sau!");
+                }
+
+                setTotalPages(data.pageInfo.totalPages);
+                setTotalRecords(data.pageInfo.totalElements);
+                setCourses(data.result);
+            } catch (error) {
+                toast.error(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
     }, [search, currentPage, recordsPerPage, flag]);
 
     return (
